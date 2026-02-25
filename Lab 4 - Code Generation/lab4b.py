d5caaf9a1b7c18d13c2936a88cb4328a1bbcd79d
Rod Davison <rod@exgnosis.ca>
2026-02-24 22:52:31 -0500

    lab 4

--- Lab 4 - Code Generation/lab4b.py
+++ Lab 4 - Code Generation/lab4b.py
@@ -1,13 +1,13 @@
-#
+
 
 from openai import OpenAI
 
 # Create client
 client = OpenAI()
 
-prompt='Create a minimal but production-quality Python 3.11 Flask web application
+prompt="""Create a minimal but production-quality Python 3.11 Flask web application
         that exposes a single HTTP GET endpoint at the root path ("/")
-        and returns the text "Hello, World!".'
+        and returns the text "Hello, World!"."""
 
 # Simple prompt
 response = client.responses.create(
